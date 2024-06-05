22fd7d09d65bb29b9e613f70ec67c54192eb14f4
safimiazi <mohibullamiazi@gmail.com>
2024-06-05 14:51:57 +0600

    dfjdskl

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,10 +1,10 @@
-import httpStatus from 'http-status';
-import mongoose from 'mongoose';
-import { TCourse, TCoursefaculty } from './course.interface';
-import { Course, CourseFaculty } from './course.model';
-import { CourseSearchableFields } from './course.constant';
-import QueryBuilder from '../../builder/queryBuilder';
-import AppError from '../../errors/appErrors';
+import httpStatus from "http-status";
+import mongoose, { mongo } from "mongoose";
+import { TCourse, TCoursefaculty } from "./course.interface";
+import { Course, CourseFaculty } from "./course.model";
+import { CourseSearchableFields } from "./course.constant";
+import QueryBuilder from "../../builder/queryBuilder";
+import AppError from "../../errors/appErrors";
 
 const createCourseIntoDB = async (payload: TCourse) => {
   const result = await Course.create(payload);
@@ -13,9 +13,8 @@ const createCourseIntoDB = async (payload: TCourse) => {
 
 const getAllCoursesFromDB = async (query: Record<string, unknown>) => {
   const courseQuery = new QueryBuilder(
-    Course.find(),
-    // .populate('preRequisiteCourses.course'),
-    query,
+    Course.find().populate("preRequisiteCourses.course"),
+    query
   )
     .search(CourseSearchableFields)
     .filter()
@@ -29,7 +28,7 @@ const getAllCoursesFromDB = async (query: Record<string, unknown>) => {
 
 const getSingleCourseFromDB = async (id: string) => {
   const result = await Course.findById(id).populate(
-    'preRequisiteCourses.course',
+    "preRequisiteCourses.course"
   );
   return result;
 };
@@ -38,31 +37,25 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
   const { preRequisiteCourses, ...courseRemainingData } = payload;
 
   const session = await mongoose.startSession();
-
   try {
     session.startTransaction();
-    //step1: basic course info update
-    const updatedBasicCourseInfo = await Course.findByIdAndUpdate(
+
+    const updatedBasicCourseIntoDB = await Course.findByIdAndUpdate(
       id,
       courseRemainingData,
-      {
-        new: true,
-        runValidators: true,
-        session,
-      },
+      { new: true, runValidators: true, session }
     );
 
-    if (!updatedBasicCourseInfo) {
-      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to update course!');
+    if (!updatedBasicCourseIntoDB) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Fail to update Course!");
     }
 
-    // check if there is any pre requisite courses to update
+    //check if there is any pre requisite course to update
     if (preRequisiteCourses && preRequisiteCourses.length > 0) {
-      // filter out the deleted fields
+      //filter out the deleted fields:
       const deletedPreRequisites = preRequisiteCourses
-        .filter((el) => el.course && el.isDeleted)
+        .filter((el) => el.course && el.isDeleted === true)
         .map((el) => el.course);
-
       const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(
         id,
         {
@@ -74,16 +67,16 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
           new: true,
           runValidators: true,
           session,
-        },
+        }
       );
 
       if (!deletedPreRequisiteCourses) {
-        throw new AppError(httpStatus.BAD_REQUEST, 'Failed to update course!');
+        throw new AppError(httpStatus.BAD_REQUEST, "Fail to update Course!");
       }
 
-      // filter out the new course fields
+      //filter out the new course fields:
       const newPreRequisites = preRequisiteCourses?.filter(
-        (el) => el.course && !el.isDeleted,
+        (el) => el.course && !el.isDeleted
       );
 
       const newPreRequisiteCourses = await Course.findByIdAndUpdate(
@@ -95,26 +88,26 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
           new: true,
           runValidators: true,
           session,
-        },
+        }
       );
 
       if (!newPreRequisiteCourses) {
-        throw new AppError(httpStatus.BAD_REQUEST, 'Failed to update course!');
+        throw new AppError(httpStatus.BAD_REQUEST, "Fail to update Course!");
       }
+
+      const result = await Course.findById(id).populate(
+        "preRequisiteCourses.course"
+      );
+
+      return result;
     }
 
-    await session.commitTransaction();
-    await session.endSession();
-
-    const result = await Course.findById(id).populate(
-      'preRequisiteCourses.course',
-    );
-
-    return result;
-  } catch (err) {
-    await session.abortTransaction();
-    await session.endSession();
-    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to update course');
+    session.commitTransaction();
+    session.endSession();
+  } catch (error) {
+    session.abortTransaction();
+    session.endSession();
+    throw new AppError(httpStatus.BAD_REQUEST, "Fail to update Course!");
   }
 };
 
@@ -124,14 +117,14 @@ const deleteCourseFromDB = async (id: string) => {
     { isDeleted: true },
     {
       new: true,
-    },
+    }
   );
   return result;
 };
 
 const assignFacultiesWithCourseIntoDB = async (
   id: string,
-  payload: Partial<TCoursefaculty>,
+  payload: Partial<TCoursefaculty>
 ) => {
   const result = await CourseFaculty.findByIdAndUpdate(
     id,
@@ -142,14 +135,14 @@ const assignFacultiesWithCourseIntoDB = async (
     {
       upsert: true,
       new: true,
-    },
+    }
   );
   return result;
 };
 
 const removeFacultiesFromCourseFromDB = async (
   id: string,
-  payload: Partial<TCoursefaculty>,
+  payload: Partial<TCoursefaculty>
 ) => {
   const result = await CourseFaculty.findByIdAndUpdate(
     id,
@@ -158,7 +151,7 @@ const removeFacultiesFromCourseFromDB = async (
     },
     {
       new: true,
-    },
+    }
   );
   return result;
 };
